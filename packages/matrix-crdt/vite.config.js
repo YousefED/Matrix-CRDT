115acf8d3c04833f0690862034cab68a7378362a
yousefed <yousefdardiry@gmail.com>
2022-08-03 07:46:05 +0200

    vite, wip: jest broken

---
+++ packages/matrix-crdt/vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      name: "matrix-crdt",
+      entry: resolve(__dirname, "src/index.ts"),
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        "yjs",
+        "vscode-lib",
+        "lib0",
+        "matrix-js-sdk",
+        "y-protocols",
+        "lodash",
+        "simple-peer",
+      ],
+    },
+  },
+});
