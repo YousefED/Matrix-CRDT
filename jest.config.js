165f299aa10dc0eb4bfd97d151154fb1b21df735
yousefed <yousefdardiry@gmail.com>
2022-08-03 08:42:07 +0200

    vitest instead of jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  //   preset: "ts-jest",
-  projects: ["<rootDir>/packages/*/jest.config.js"],
-  testEnvironment: "jsdom",
-  // roots: ["<rootDir>"],
-  // modulePaths: ["<rootDir>"],
-  // moduleNameMapper: {
-  // },
-};
--- packages/matrix-crdt/vite.config.js
+++ packages/matrix-crdt/vite.config.js
@@ -21,4 +21,7 @@ export default defineConfig({
       ],
     },
   },
+  test: {
+    setupFiles: "src/setupTests.ts",
+  },
 });
