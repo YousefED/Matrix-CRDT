bc516cf81d7d0aac82fcca8b8d4c61174ab35737
yousefed <yousefdardiry@gmail.com>
2023-01-15 21:25:33 +0100

    fix

--- examples/todo-simple-react-vite/vite.config.ts
+++ examples/todo-simple-react-vite/vite.config.ts
@@ -1,9 +1,23 @@
 import react from "@vitejs/plugin-react";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // required for @primer/react
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    minify: false,
+    // needed for matrix-js-sdk:
+    rollupOptions: {
+      // Enable rollup polyfills plugin
+      // used during production bundling
+      plugins: [nodePolyfills()],
+    },
+  },
   // needed for matrix-js-sdk:
   define: { global: "window" },
 });
